d600564419b884adbc6cae5e0a98c6149f10c6d0
rodrigo0567 <125127942+rodrigo0567@users.noreply.github.com>
2023-11-02 23:49:45 -0300

    TelaPrincipal.java

--- RealTimeGPSMapApp.java
+++ RealTimeGPSMapApp.java
@@ -1,60 +1,70 @@
 package Avaliação_POO;
 
 import javax.swing.*;
-import com.google.maps.GeoApiContext;
-import com.google.maps.StaticMapsApi;
-import com.google.maps.model.LatLng;
+import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
-public class RealTimeGPSTrackingApp {
+public class TelaPrincipal extends JFrame {
+
+    public TelaPrincipal(String nomeUsuario) {
+        setTitle("Tela Principal");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+        setLayout(new BorderLayout());
+
+        Color azulMarinho = new Color(0, 0, 102);
+        getContentPane().setBackground(azulMarinho);
+
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.setBackground(Color.WHITE);
+        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        JLabel labelDataHora = new JLabel();
+        labelDataHora.setForeground(Color.BLACK);
+
+        panel.add(labelDataHora, BorderLayout.NORTH);
+        add(panel, BorderLayout.NORTH);
+
+        SimpleDateFormat dataFormatada = new SimpleDateFormat("dd/MM/yyyy");
+        DateTimeFormatter horaFormatada = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+        Timer timer = new Timer(1000, e -> {
+            Date data = new Date();
+            labelDataHora.setText(dataFormatada.format(data) + " " + LocalTime.now().format(horaFormatada));
+        });
+        timer.start();
+
+        JButton btnChat = new JButton("Chat");
+        btnChat.setBackground(Color.CYAN);
+        btnChat.setForeground(Color.WHITE);
+        btnChat.addActionListener(e -> {
+            dispose();
+            SwingUtilities.invokeLater(() -> {
+                ChatAluno chatAluno = new ChatAluno(nomeUsuario);
+            });
+        });
+        add(btnChat, BorderLayout.CENTER);
+
+        JButton btnSair = new JButton("Sair");
+        btnSair.setBackground(Color.CYAN);
+        btnSair.setForeground(Color.WHITE);
+        btnSair.addActionListener(e -> {
+            dispose(); // Fecha a janela da TelaPrincipal
+            SwingUtilities.invokeLater(() -> {
+                TelaDeLogin telaDeLogin = new TelaDeLogin();
+                telaDeLogin.setVisible(true);
+            });
+        });
+        add(btnSair, BorderLayout.SOUTH);
+    }
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Real-Time GPS Tracking App");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(800, 600);
-
-            GeoApiContext context = new GeoApiContext.Builder()
-                    .apiKey("SUA_CHAVE_DA_API_DO_GOOGLE_MAPS")
-                    .build();
-
-            Timer timer = new Timer(1000, e -> {
-                // Simule a obtenção das coordenadas de GPS em tempo real
-                double latitude = 40.7128 + (Math.random() - 0.5) * 0.01;
-                double longitude = -74.0060 + (Math.random() - 0.5) * 0.01;
-
-                // Atualize o mapa com as novas coordenadas
-                LatLng location = new LatLng(latitude, longitude);
-                ImageIcon mapImage = getMapImage(context, location);
-
-                JLabel mapLabel = new JLabel(mapImage);
-                frame.getContentPane().removeAll();
-                frame.getContentPane().add(mapLabel);
-                frame.revalidate();
-                frame.repaint();
-            });
-            timer.start();
-
-            frame.setVisible(true);
+            TelaPrincipal telaPrincipal = new TelaPrincipal("NomeDoUsuario");
+            telaPrincipal.setVisible(true);
         });
     }
-
-    private static ImageIcon getMapImage(GeoApiContext context, LatLng location) {
-        try {
-            String staticMapUrl = StaticMapsApi.newRequest(context)
-                    .center(location)
-                    .zoom(10)
-                    .size(new com.google.maps.model.Size(800, 600))
-                    .scale(2)
-                    .toString();
-
-            // Carregar a imagem do URL
-            java.net.URL imageUrl = new java.net.URL(staticMapUrl);
-            java.awt.Image image = java.awt.Toolkit.getDefaultToolkit().getDefaultToolkit().createImage(imageUrl);
-
-            return new ImageIcon(image);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
 }
-
